2fa2c28573c359d92446d2cf2cd2fc067fa2b7bd
Amadou Diatta <amadou13.diatta@gmail.com>
2022-08-31 01:39:13 +0000

    cloudinary

--- prixetudiant/settings.py
+++ prixetudiant/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'contact.apps.ContactConfig',
     'products.apps.ProductsConfig',
     'cart.apps.CartConfig',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
