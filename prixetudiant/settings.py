61e66918132afe6e05fd52294b489513caa6eadf
Amadou Diatta <amadou13.diatta@gmail.com>
2022-08-31 01:43:21 +0000

    cloudinary

--- prixetudiant/settings.py
+++ prixetudiant/settings.py
@@ -137,6 +137,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
